76b2f288601871e6e635bcb19469d521b9aea535
Mr. Luyk <133881094+MrLuyk@users.noreply.github.com>
2024-10-16 17:59:42 +0000

    Added switch cases 0-9

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Choose a number 0-10");
+        int x = int.Parse(Console.ReadLine());
+
+        switch (x)
+        {
+            case 0:
+                Console.WriteLine("Your number is Zeto");
+                break;
+            case 1:
+                Console.WriteLine("Your number is One");
+                break;
+            case 2:
+                Console.WriteLine("Your number is Two");
+                break;
+            case 3:
+                Console.WriteLine("Your number is Three");
+                break;
+            case 4:
+                Console.WriteLine("Your number is Four");
+                break;
+            case 5:
+                Console.WriteLine("Your number is Five");
+                break;
+            case 6:
+                Console.WriteLine("Your number is Six");
+                break;
+            case 7:
+                Console.WriteLine("Your number is Seven");
+                break;
+            case 8:
+                Console.WriteLine("Your number is Eight");
+                break;
+            case 9:
+                Console.WriteLine("Your number is Nine");
+                break;
+        }
+    }
+}
