34c75a56cd59e3decb63f110ad8f27da48d584b7
Mr. Luyk <133881094+MrLuyk@users.noreply.github.com>
2024-10-17 13:36:54 +0000

    Added cases 11-100

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Choose a number 0-10");
+        Console.WriteLine("Choose a number 0-100");
         int x = int.Parse(Console.ReadLine());
 
         switch (x)
@@ -38,6 +38,279 @@ public class Program
                 break;
             case 9:
                 Console.WriteLine("Your number is Nine");
+                break;
+            case 10:
+                Console.WriteLine("Your number is Ten");
+                break;
+            case 11:
+                Console.WriteLine("Your number is Eleven");
+                break;
+            case 12:
+                Console.WriteLine("Your number is Twelve");
+                break;
+            case 13:
+                Console.WriteLine("Your number is Thirteen");
+                break;
+            case 14:
+                Console.WriteLine("Your number is Fourteen");
+                break;
+            case 15:
+                Console.WriteLine("Your number is Fifteen");
+                break;
+            case 16:
+                Console.WriteLine("Your number is Sixteen");
+                break;
+            case 17:
+                Console.WriteLine("Your number is Seventeen");
+                break;
+            case 18:
+                Console.WriteLine("Your number is Eighteen");
+                break;
+            case 19:
+                Console.WriteLine("Your number is Nineteen");
+                break;
+            case 20:
+                Console.WriteLine("Your number is Twenty");
+                break;
+            case 21:
+                Console.WriteLine("Your number is Twenty-One");
+                break;
+            case 22:
+                Console.WriteLine("Your number is Twenty-Two");
+                break;
+            case 23:
+                Console.WriteLine("Your number is Twenty-Three");
+                break;
+            case 24:
+                Console.WriteLine("Your number is Twenty-Four");
+                break;
+            case 25:
+                Console.WriteLine("Your number is Twenty-Five");
+                break;
+            case 26:
+                Console.WriteLine("Your number is Twenty-Six");
+                break;
+            case 27:
+                Console.WriteLine("Your number is Twenty-Seven");
+                break;
+            case 28:
+                Console.WriteLine("Your number is Twenty-Eight");
+                break;
+            case 29:
+                Console.WriteLine("Your number is Twenty-Nine");
+                break;
+            case 30:
+                Console.WriteLine("Your number is Thirty");
+                break;
+            case 31:
+                Console.WriteLine("Your number is Thirty-One");
+                break;
+            case 32:
+                Console.WriteLine("Your number is Thirty-Two");
+                break;
+            case 33:
+                Console.WriteLine("Your number is Thirty-Three");
+                break;
+            case 34:
+                Console.WriteLine("Your number is Thirty-Four");
+                break;
+            case 35:
+                Console.WriteLine("Your number is Thirty-Five");
+                break;
+            case 36:
+                Console.WriteLine("Your number is Thirty-Six");
+                break;
+            case 37:
+                Console.WriteLine("Your number is Thirty-Seven");
+                break;
+            case 38:
+                Console.WriteLine("Your number is Thirty-Eight");
+                break;
+            case 39:
+                Console.WriteLine("Your number is Thirty-Nine");
+                break;
+            case 40:
+                Console.WriteLine("Your number is Fourty");
+                break;
+            case 41:
+                Console.WriteLine("Your number is Fourty-One");
+                break;
+            case 42:
+                Console.WriteLine("Your number is Fourty-Two");
+                break;
+            case 43:
+                Console.WriteLine("Your number is Fourty-Three");
+                break;
+            case 44:
+                Console.WriteLine("Your number is Fourty-Four");
+                break;
+            case 45:
+                Console.WriteLine("Your number is Fourty-Five");
+                break;
+            case 46:
+                Console.WriteLine("Your number is Fourty-Six");
+                break;
+            case 47:
+                Console.WriteLine("Your number is Fourty-Seven");
+                break;
+            case 48:
+                Console.WriteLine("Your number is Fourty-Eight");
+                break;
+            case 49:
+                Console.WriteLine("Your number is Fourty-Nine");
+                break;
+            case 50:
+                Console.WriteLine("Your number is Fifty");
+                break;
+            case 51:
+                Console.WriteLine("Your number is Fifty-One");
+                break;
+            case 52:
+                Console.WriteLine("Your number is Fifty-Two");
+                break;
+            case 53:
+                Console.WriteLine("Your number is Fifty-Three");
+                break;
+            case 54:
+                Console.WriteLine("Your number is Fifty-Four");
+                break;
+            case 55:
+                Console.WriteLine("Your number is Fifty-Five");
+                break;
+            case 56:
+                Console.WriteLine("Your number is Fifty-Six");
+                break;
+            case 57:
+                Console.WriteLine("Your number is Fifty-Seven");
+                break;
+            case 58:
+                Console.WriteLine("Your number is Fifty-Eight");
+                break;
+            case 59:
+                Console.WriteLine("Your number is Fifty-Nine");
+                break;
+            case 60:
+                Console.WriteLine("Your number is Sixty");
+                break;
+            case 61:
+                Console.WriteLine("Your number is Sixty-One");
+                break;
+            case 62:
+                Console.WriteLine("Your number is Sixty-Two");
+                break;
+            case 63:
+                Console.WriteLine("Your number is Sixty-Three");
+                break;
+            case 64:
+                Console.WriteLine("Your number is Sixty-Four");
+                break;
+            case 65:
+                Console.WriteLine("Your number is Sixty-Five");
+                break;
+            case 66:
+                Console.WriteLine("Your number is Sixty-Six");
+                break;
+            case 67:
+                Console.WriteLine("Your number is Sixty-Seven");
+                break;
+            case 68:
+                Console.WriteLine("Your number is Sixty-Eight");
+                break;
+            case 69:
+                Console.WriteLine("Your number is Sixty-Nine");
+                break;
+            case 70:
+                Console.WriteLine("Your number is Seventy");
+                break;
+            case 71:
+                Console.WriteLine("Your number is Seventy-One");
+                break;
+            case 72:
+                Console.WriteLine("Your number is Seventy-Two");
+                break;
+            case 73:
+                Console.WriteLine("Your number is Seventy-Three");
+                break;
+            case 74:
+                Console.WriteLine("Your number is Seventy-Four");
+                break;
+            case 75:
+                Console.WriteLine("Your number is Seventy-Five");
+                break;
+            case 76:
+                Console.WriteLine("Your number is Seventy-Six");
+                break;
+            case 77:
+                Console.WriteLine("Your number is Seventy-Seven");
+                break;
+            case 78:
+                Console.WriteLine("Your number is Seventy-Eight");
+                break;
+            case 79:
+                Console.WriteLine("Your number is Seventy-Nine");
+                break;
+            case 80:
+                Console.WriteLine("Your number is Eighty");
+                break;
+            case 81:
+                Console.WriteLine("Your number is Eighty-One");
+                break;
+            case 82:
+                Console.WriteLine("Your number is Eighty-Two");
+                break;
+            case 83:
+                Console.WriteLine("Your number is Eighty-Three");
+                break;
+            case 84:
+                Console.WriteLine("Your number is Eighty-Four");
+                break;
+            case 85:
+                Console.WriteLine("Your number is Eighty-Five");
+                break;
+            case 86:
+                Console.WriteLine("Your number is Eighty-Six");
+                break;
+            case 87:
+                Console.WriteLine("Your number is Eighty-Seven");
+                break;
+            case 88:
+                Console.WriteLine("Your number is Eighty-Eight");
+                break;
+            case 89:
+                Console.WriteLine("Your number is Eighty-Nine");
+                break;
+            case 90:
+                Console.WriteLine("Your number is Ninety");
+                break;
+            case 91:
+                Console.WriteLine("Your number is Ninety-One");
+                break;
+            case 92:
+                Console.WriteLine("Your number is Ninety-Two");
+                break;
+            case 93:
+                Console.WriteLine("Your number is Ninety-Three");
+                break;
+            case 94:
+                Console.WriteLine("Your number is Ninety-Four");
+                break;
+            case 95:
+                Console.WriteLine("Your number is Ninety-Five");
+                break;
+            case 96:
+                Console.WriteLine("Your number is Ninety-Six");
+                break;
+            case 97:
+                Console.WriteLine("Your number is Ninety-Seven");
+                break;
+            case 98:
+                Console.WriteLine("Your number is Ninety-Eight");
+                break;
+            case 99:
+                Console.WriteLine("Your number is Ninety-Nine");
+                break;
+            case 100:
+                Console.WriteLine("Your number is One-Hundred!");
                 break;
         }
     }
