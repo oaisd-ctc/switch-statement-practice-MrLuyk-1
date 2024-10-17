6b9bd29e46e9162b5ed4a3430a3dea724f12fdb6
Mr. Luyk <133881094+MrLuyk@users.noreply.github.com>
2024-10-17 13:46:17 +0000

    Added cases 101-200. Removed '-' from One-Hundred.

--- Program.cs
+++ Program.cs
@@ -311,7 +311,307 @@ public class Program
                 Console.WriteLine("Your number is Ninety-Nine");
                 break;
             case 100:
-                Console.WriteLine("Your number is One-Hundred!");
+                Console.WriteLine("Your number is One Hundred!");
+                break;
+            case 101:
+                Console.WriteLine("Your number is One Hundred and One");
+                break;
+            case 102:
+                Console.WriteLine("Your number is One Hundred and Two");
+                break;
+            case 103:
+                Console.WriteLine("Your number is One Hundred and Three");
+                break;
+            case 104:
+                Console.WriteLine("Your number is One Hundred and Four");
+                break;
+            case 105:
+                Console.WriteLine("Your number is One Hundred and Five");
+                break;
+            case 106:
+                Console.WriteLine("Your number is One Hundred and Six");
+                break;
+            case 107:
+                Console.WriteLine("Your number is One Hundred and Seven");
+                break;
+            case 108:
+                Console.WriteLine("Your number is One Hundred and Eight");
+                break;
+            case 109:
+                Console.WriteLine("Your number is One Hundred and Nine");
+                break;
+            case 110:
+                Console.WriteLine("Your number is One Hundred and Ten");
+                break;
+            case 111:
+                Console.WriteLine("Your number is One Hundred and Eleven");
+                break;
+            case 112:
+                Console.WriteLine("Your number is One Hundred and Twelve");
+                break;
+            case 113:
+                Console.WriteLine("Your number is One Hundred and Thirteen");
+                break;
+            case 114:
+                Console.WriteLine("Your number is One Hundred and Fourteen");
+                break;
+            case 115:
+                Console.WriteLine("Your number is One Hundred and Fifteen");
+                break;
+            case 116:
+                Console.WriteLine("Your number is One Hundred and Sixteen");
+                break;
+            case 117:
+                Console.WriteLine("Your number is One Hundred and Seventeen");
+                break;
+            case 118:
+                Console.WriteLine("Your number is One Hundred and Eighteen");
+                break;
+            case 119:
+                Console.WriteLine("Your number is One Hundred and Nineteen");
+                break;
+            case 120:
+                Console.WriteLine("Your number is One Hundred and Twenty");
+                break;
+            case 121:
+                Console.WriteLine("Your number is One Hundred and Twenty-One");
+                break;
+            case 122:
+                Console.WriteLine("Your number is One Hundred and Twenty-Two");
+                break;
+            case 123:
+                Console.WriteLine("Your number is One Hundred and Twenty-Three");
+                break;
+            case 124:
+                Console.WriteLine("Your number is One Hundred and Twenty-Four");
+                break;
+            case 125:
+                Console.WriteLine("Your number is One Hundred and Twenty-Five");
+                break;
+            case 126:
+                Console.WriteLine("Your number is One Hundred and Twenty-Six");
+                break;
+            case 127:
+                Console.WriteLine("Your number is One Hundred and Twenty-Seven");
+                break;
+            case 128:
+                Console.WriteLine("Your number is One Hundred and Twenty-Eight");
+                break;
+            case 129:
+                Console.WriteLine("Your number is One Hundred and Twenty-Nine");
+                break;
+            case 130:
+                Console.WriteLine("Your number is One Hundred and Thirty");
+                break;
+            case 131:
+                Console.WriteLine("Your number is One Hundred and Thirty-One");
+                break;
+            case 132:
+                Console.WriteLine("Your number is One Hundred and Thirty-Two");
+                break;
+            case 133:
+                Console.WriteLine("Your number is One Hundred and Thirty-Three");
+                break;
+            case 134:
+                Console.WriteLine("Your number is One Hundred and Thirty-Four");
+                break;
+            case 135:
+                Console.WriteLine("Your number is One Hundred and Thirty-Five");
+                break;
+            case 136:
+                Console.WriteLine("Your number is One Hundred and Thirty-Six");
+                break;
+            case 137:
+                Console.WriteLine("Your number is One Hundred and Thirty-Seven");
+                break;
+            case 138:
+                Console.WriteLine("Your number is One Hundred and Thirty-Eight");
+                break;
+            case 139:
+                Console.WriteLine("Your number is One Hundred and Thirty-Nine");
+                break;
+            case 140:
+                Console.WriteLine("Your number is One Hundred and Fourty");
+                break;
+            case 141:
+                Console.WriteLine("Your number is One Hundred and Fourty-One");
+                break;
+            case 142:
+                Console.WriteLine("Your number is One Hundred and Fourty-Two");
+                break;
+            case 143:
+                Console.WriteLine("Your number is One Hundred and Fourty-Three");
+                break;
+            case 144:
+                Console.WriteLine("Your number is One Hundred and Fourty-Four");
+                break;
+            case 145:
+                Console.WriteLine("Your number is One Hundred and Fourty-Five");
+                break;
+            case 146:
+                Console.WriteLine("Your number is One Hundred and Fourty-Six");
+                break;
+            case 147:
+                Console.WriteLine("Your number is One Hundred and Fourty-Seven");
+                break;
+            case 148:
+                Console.WriteLine("Your number is One Hundred and Fourty-Eight");
+                break;
+            case 149:
+                Console.WriteLine("Your number is One Hundred and Fourty-Nine");
+                break;
+            case 150:
+                Console.WriteLine("Your number is One Hundred and Fifty");
+                break;
+            case 151:
+                Console.WriteLine("Your number is One Hundred and Fifty-One");
+                break;
+            case 152:
+                Console.WriteLine("Your number is One Hundred and Fifty-Two");
+                break;
+            case 153:
+                Console.WriteLine("Your number is One Hundred and Fifty-Three");
+                break;
+            case 154:
+                Console.WriteLine("Your number is One Hundred and Fifty-Four");
+                break;
+            case 155:
+                Console.WriteLine("Your number is One Hundred and Fifty-Five");
+                break;
+            case 156:
+                Console.WriteLine("Your number is One Hundred and Fifty-Six");
+                break;
+            case 157:
+                Console.WriteLine("Your number is One Hundred and Fifty-Seven");
+                break;
+            case 158:
+                Console.WriteLine("Your number is One Hundred and Fifty-Eight");
+                break;
+            case 159:
+                Console.WriteLine("Your number is One Hundred and Fifty-Nine");
+                break;
+            case 160:
+                Console.WriteLine("Your number is One Hundred and Sixty");
+                break;
+            case 161:
+                Console.WriteLine("Your number is One Hundred and Sixty-One");
+                break;
+            case 162:
+                Console.WriteLine("Your number is One Hundred and Sixty-Two");
+                break;
+            case 163:
+                Console.WriteLine("Your number is One Hundred and Sixty-Three");
+                break;
+            case 164:
+                Console.WriteLine("Your number is One Hundred and Sixty-Four");
+                break;
+            case 165:
+                Console.WriteLine("Your number is One Hundred and Sixty-Five");
+                break;
+            case 166:
+                Console.WriteLine("Your number is One Hundred and Sixty-Six");
+                break;
+            case 167:
+                Console.WriteLine("Your number is One Hundred and Sixty-Seven");
+                break;
+            case 168:
+                Console.WriteLine("Your number is One Hundred and Sixty-Eight");
+                break;
+            case 169:
+                Console.WriteLine("Your number is One Hundred and Sixty-Nine");
+                break;
+            case 170:
+                Console.WriteLine("Your number is One Hundred and Seventy");
+                break;
+            case 171:
+                Console.WriteLine("Your number is One Hundred and Seventy-One");
+                break;
+            case 172:
+                Console.WriteLine("Your number is One Hundred and Seventy-Two");
+                break;
+            case 173:
+                Console.WriteLine("Your number is One Hundred and Seventy-Three");
+                break;
+            case 174:
+                Console.WriteLine("Your number is One Hundred and Seventy-Four");
+                break;
+            case 175:
+                Console.WriteLine("Your number is One Hundred and Seventy-Five");
+                break;
+            case 176:
+                Console.WriteLine("Your number is One Hundred and Seventy-Six");
+                break;
+            case 177:
+                Console.WriteLine("Your number is One Hundred and Seventy-Seven");
+                break;
+            case 178:
+                Console.WriteLine("Your number is One Hundred and Seventy-Eight");
+                break;
+            case 179:
+                Console.WriteLine("Your number is One Hundred and Seventy-Nine");
+                break;
+            case 180:
+                Console.WriteLine("Your number is One Hundred and Eighty");
+                break;
+            case 181:
+                Console.WriteLine("Your number is One Hundred and Eighty-One");
+                break;
+            case 182:
+                Console.WriteLine("Your number is One Hundred and Eighty-Two");
+                break;
+            case 183:
+                Console.WriteLine("Your number is One Hundred and Eighty-Three");
+                break;
+            case 184:
+                Console.WriteLine("Your number is One Hundred and Eighty-Four");
+                break;
+            case 185:
+                Console.WriteLine("Your number is One Hundred and Eighty-Five");
+                break;
+            case 186:
+                Console.WriteLine("Your number is One Hundred and Eighty-Six");
+                break;
+            case 187:
+                Console.WriteLine("Your number is One Hundred and Eighty-Seven");
+                break;
+            case 188:
+                Console.WriteLine("Your number is One Hundred and Eighty-Eight");
+                break;
+            case 189:
+                Console.WriteLine("Your number is One Hundred and Eighty-Nine");
+                break;
+            case 190:
+                Console.WriteLine("Your number is One Hundred and Ninety");
+                break;
+            case 191:
+                Console.WriteLine("Your number is One Hundred and Ninety-One");
+                break;
+            case 192:
+                Console.WriteLine("Your number is One Hundred and Ninety-Two");
+                break;
+            case 193:
+                Console.WriteLine("Your number is One Hundred and Ninety-Three");
+                break;
+            case 194:
+                Console.WriteLine("Your number is One Hundred and Ninety-Four");
+                break;
+            case 195:
+                Console.WriteLine("Your number is One Hundred and Ninety-Five");
+                break;
+            case 196:
+                Console.WriteLine("Your number is One Hundred and Ninety-Six");
+                break;
+            case 197:
+                Console.WriteLine("Your number is One Hundred and Ninety-Seven");
+                break;
+            case 198:
+                Console.WriteLine("Your number is One Hundred and Ninety-Eight");
+                break;
+            case 199:
+                Console.WriteLine("Your number is One Hundred and Ninety-Nine");
+                break;
+            case 200:
+                Console.WriteLine("Your number is Two Hundred!");
                 break;
         }
     }
